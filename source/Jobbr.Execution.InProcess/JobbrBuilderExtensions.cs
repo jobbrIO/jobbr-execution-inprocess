bcdf83dc09875f8902aca524769884eda6df98e4
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-09-29 10:17:04 +0200

    dummy implementation added

---
+++ source/Jobbr.Execution.InProcess/JobbrBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Jobbr.ComponentModel.Registration;
+
+namespace Jobbr.Execution.InMemory
+{
+    public static class JobbrBuilderExtensions
+    {
+        public static IJobbrBuilder AddInProcessExecutor()
+        {
+            throw  new NotImplementedException();
+        }
+    }
+}
